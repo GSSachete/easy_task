05bcdfdab886b0d014f820e4a09b4901024264a9
GSSachete <gabrielasachete@outlook.com>
2024-10-19 12:57:30 -0300

    RotasLogin

--- login/views.py
+++ login/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def login(request):
+    return render(request, 'login/login.html')
+def register(request):
+    return render(request, 'login/register.html')
