a8166c90c34343f0f7f68e984ed69a49a4f291e0
GSSachete <gabrielasachete@outlook.com>
2024-10-22 13:36:56 -0300

    Front_Register
    Feito o front e ajustes de model e novos campos

--- login/forms.py
+++ login/forms.py
@@ -1,15 +1,46 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms.widgets import DateInput
+from .models import Usuario  # Substitua YourModel por Usuario
+from django import forms
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class RegisterForm(forms.ModelForm):
+    data_nascimento = forms.DateField(widget=DateInput(attrs={'type': 'date'}))
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = Usuario
+        fields = ['nome', 'escolaridade', 'data_nascimento', 'escola', 'email', 'senha']
+
+class RegisterForm(forms.ModelForm):
+    data_nascimento = forms.DateField(widget=DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Usuario
+        fields = ['nome', 'escolaridade', 'data_nascimento', 'escola', 'email', 'senha']
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Email / Usuario')
     password = forms.CharField(widget=forms.PasswordInput)
+    
+class RegisterForm(forms.Form):
+    NIVEIS_ESCOLARIDADE = [
+        ('0', 'Primário'),
+        ('1', 'Ensino Fundamental'),
+        ('2', 'Ensino Médio'),
+        ('3', 'Ensino Técnico'),
+        ('4', 'Ensino Superior'),
+        ('5', 'Pós-graduação'),
+        ('6', 'Mestrado'),
+        ('7', 'Doutorado'),
+    ]
+
+
+    nome = forms.CharField(max_length=100)
+    escolaridade = forms.ChoiceField(choices=NIVEIS_ESCOLARIDADE)
+    data_nascimento = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    escola = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    senha = forms.CharField(widget=forms.PasswordInput)
+    
--- login/models.py
+++ login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    escolaridade = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    escola = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
