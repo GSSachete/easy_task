b2401834850db76b256614e60b19acdaa24e369f
JoaoPedroBMoraes <jpbuenomoraes3@gmail.com>
2024-11-06 18:01:16 -0300

    problema anexar arquivo corrigido

--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', lambda request: redirect('login')),
 ]
 
-# Adicione isso para servir arquivos de mídia durante o desenvolvimento
+# Adicione esta linha para servir arquivos de mídia durante o desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
